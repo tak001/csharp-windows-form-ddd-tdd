dfea67f35692896487744f22f3f571dad8769757
tak001 <twitwitwi2@yahoo.co.jp>
2022-01-30 21:52:26 +0900

    feat:BADコード改善1

---
+++ DDD.WinForm/Common/CommonConst.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD.WinForm.Common
+{
+    public static class CommonConst
+    {
+        public const string TemperatureUnitName = "℃";
+        public const int TemperatureDecimalPoint = 2;
+        public const string ConnectionString = @"Data Source=C:\Users\admin\task2\DDD\DDD.db;Version=3;";
+    }
+}
--- DDD.WinForm/WeatherLatestView.cs
+++ DDD.WinForm/WeatherLatestView.cs
@@ -9,13 +9,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DDD.WinForm.Common;
+using DDD.WinForm.Common.Data;
 
 namespace DDD.WinForm
 {
     public partial class WeatherLatestView : Form
     {
-        private readonly string ConnectionString = @"Data Source=C:\Users\admin\task2\DDD\DDD.db;Version=3;";
-
         public WeatherLatestView()
         {
             InitializeComponent();
@@ -23,42 +23,18 @@ namespace DDD.WinForm
 
         private void LatestButton_Click(object sender, EventArgs e)
         {
-            string sql = @"
-select DataDate,
-        Condition,
-        Temperature
-from Weather
-where AreaId = @AreaId
-order by DataDate desc
-LIMIT 1
-";
-
-            DataTable dt = new DataTable();
-            using (var connection = new SQLiteConnection(ConnectionString))
-            using (var command = new SQLiteCommand(sql, connection))
-            {
-                connection.Open();
-
-                command.Parameters.AddWithValue("@AreaId", this.AreaIdTextBox.Text);
-                using (var adapter = new SQLiteDataAdapter(command))
-                {
-                    adapter.Fill(dt);
-                }
-            }
+            var dt = WeatherSQLite.GetLatest(Convert.ToInt32(AreaIdTextBox.Text));
 
             if (dt.Rows.Count > 0)
             {
                 DataDateLabel.Text = dt.Rows[0]["DataDate"].ToString();
                 ConditionLabel.Text = dt.Rows[0]["Condition"].ToString();
                 TemperatureLabel.Text =
-                    RoundString(Convert.ToSingle(dt.Rows[0]["Temperature"].ToString()), 2) + "℃";
+                   CommonFunc.RoundString(
+                        Convert.ToSingle(dt.Rows[0]["Temperature"].ToString()),
+                        CommonConst.TemperatureDecimalPoint)
+                        + CommonConst.TemperatureUnitName;
             }
-        }
-
-        private string RoundString(float value, int decimalPoint)
-        {
-            var temp = Convert.ToSingle(Math.Round(value, decimalPoint));
-            return temp.ToString("F" + decimalPoint);
         }
 
     }
