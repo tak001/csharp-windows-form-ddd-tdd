5de344cd6f3423fc31f91d8ddd539fa6b36f9027
tak001 <twitwitwi2@yahoo.co.jp>
2022-01-30 19:22:50 +0900

    windows form 最初のコード

---
+++ DDD.WinForm/WeatherLatestView.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace DDD.WinForm
+{
+    partial class WeatherLatestView
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.DataDateLabel = new System.Windows.Forms.Label();
+            this.ConditionLabel = new System.Windows.Forms.Label();
+            this.TemperatureLabel = new System.Windows.Forms.Label();
+            this.AreaIdTextBox = new System.Windows.Forms.TextBox();
+            this.LatestButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "地域";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "日時";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "状態";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "温度";
+            // 
+            // DataDateLabel
+            // 
+            this.DataDateLabel.AutoSize = true;
+            this.DataDateLabel.Location = new System.Drawing.Point(85, 71);
+            this.DataDateLabel.Name = "DataDateLabel";
+            this.DataDateLabel.Size = new System.Drawing.Size(35, 12);
+            this.DataDateLabel.TabIndex = 4;
+            this.DataDateLabel.Text = "label5";
+            // 
+            // ConditionLabel
+            // 
+            this.ConditionLabel.AutoSize = true;
+            this.ConditionLabel.Location = new System.Drawing.Point(85, 104);
+            this.ConditionLabel.Name = "ConditionLabel";
+            this.ConditionLabel.Size = new System.Drawing.Size(35, 12);
+            this.ConditionLabel.TabIndex = 5;
+            this.ConditionLabel.Text = "label6";
+            // 
+            // TemperatureLabel
+            // 
+            this.TemperatureLabel.AutoSize = true;
+            this.TemperatureLabel.Location = new System.Drawing.Point(85, 139);
+            this.TemperatureLabel.Name = "TemperatureLabel";
+            this.TemperatureLabel.Size = new System.Drawing.Size(35, 12);
+            this.TemperatureLabel.TabIndex = 6;
+            this.TemperatureLabel.Text = "label7";
+            // 
+            // AreaIdTextBox
+            // 
+            this.AreaIdTextBox.Location = new System.Drawing.Point(87, 42);
+            this.AreaIdTextBox.Name = "AreaIdTextBox";
+            this.AreaIdTextBox.Size = new System.Drawing.Size(100, 19);
+            this.AreaIdTextBox.TabIndex = 7;
+            // 
+            // LatestButton
+            // 
+            this.LatestButton.Location = new System.Drawing.Point(194, 42);
+            this.LatestButton.Name = "LatestButton";
+            this.LatestButton.Size = new System.Drawing.Size(75, 23);
+            this.LatestButton.TabIndex = 8;
+            this.LatestButton.Text = "直近地";
+            this.LatestButton.UseVisualStyleBackColor = true;
+            this.LatestButton.Click += new System.EventHandler(this.LatestButton_Click);
+            // 
+            // WeatherLatestView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.LatestButton);
+            this.Controls.Add(this.AreaIdTextBox);
+            this.Controls.Add(this.TemperatureLabel);
+            this.Controls.Add(this.ConditionLabel);
+            this.Controls.Add(this.DataDateLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "WeatherLatestView";
+            this.Text = "WeatherLatestView";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label DataDateLabel;
+        private System.Windows.Forms.Label ConditionLabel;
+        private System.Windows.Forms.Label TemperatureLabel;
+        private System.Windows.Forms.TextBox AreaIdTextBox;
+        private System.Windows.Forms.Button LatestButton;
+    }
+}
+
---
+++ DDD.WinForm/WeatherLatestView.cs
@@ -0,0 +1,59 @@
+﻿using System.Data.SQLite;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DDD.WinForm
+{
+    public partial class WeatherLatestView : Form
+    {
+        private readonly string ConnectionString = @"Data Source=C:\Users\admin\task2\DDD\DDD.db;Version=3;";
+
+        public WeatherLatestView()
+        {
+            InitializeComponent();
+        }
+
+        private void LatestButton_Click(object sender, EventArgs e)
+        {
+            string sql = @"
+select DataDate,
+        Condition,
+        Temperature
+from Weather
+where AreaId = @AreaId
+order by DataDate desc
+LIMIT 1
+";
+
+            DataTable dt = new DataTable();
+            using (var connection = new SQLiteConnection(ConnectionString))
+            using (var command = new SQLiteCommand(sql, connection))
+            {
+                connection.Open();
+
+                command.Parameters.AddWithValue("@AreaId", this.AreaIdTextBox.Text);
+                using (var adapter = new SQLiteDataAdapter(command))
+                {
+                    adapter.Fill(dt);
+                }
+            }
+
+            if (dt.Rows.Count > 0)
+            {
+                DataDateLabel.Text = dt.Rows[0]["DataDate"].ToString();
+                ConditionLabel.Text = dt.Rows[0]["Condition"].ToString();
+                TemperatureLabel.Text =
+                    Math.Round(Convert.ToSingle(dt.Rows[0]["Temperature"].ToString()), 2) + "℃";
+            }
+        }
+
+    }
+}
