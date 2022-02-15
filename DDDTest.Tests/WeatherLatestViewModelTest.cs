db9522480abd97cd4c4767654068b89c229a02b9
tak001 <twitwitwi2@yahoo.co.jp>
2022-02-15 21:50:40 +0900

    chore

--- DDDTest.Tests/WeatherLatestViewModelTest.cs
+++ DDDTest.Tests/WeatherLatestViewModelTest.cs
@@ -2,8 +2,8 @@
 using DDD.Domain.Repositories;
 using DDD.WinForm.ViewModels;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using System;
-using System.Data;
 
 namespace DDDTest.Tests
 {
@@ -13,7 +13,8 @@ namespace DDDTest.Tests
         [TestMethod]
         public void シナリオ()
         {
-            var viewModel = new WeatherLatestViewModel(new WeatherMock());
+            var weatherMock = new Mock<IWeatherRepository>();
+            var viewModel = new WeatherLatestViewModel(weatherMock.Object);
             // 初期値は全てから文字である
             Assert.AreEqual("", viewModel.AreaIdText);
             Assert.AreEqual("", viewModel.DataDateText);
@@ -26,6 +27,13 @@ namespace DDDTest.Tests
             Assert.AreEqual("2018/01/01 12:34:56", viewModel.DataDateText);
             Assert.AreEqual("曇り", viewModel.ConditionText);
             Assert.AreEqual("12.30 ℃", viewModel.TemperatureText);
+
+            viewModel.AreaIdText = "2";
+            viewModel.Search();
+            Assert.AreEqual("2", viewModel.AreaIdText);
+            Assert.AreEqual("2018/01/02 12:34:56", viewModel.DataDateText);
+            Assert.AreEqual("晴れ", viewModel.ConditionText);
+            Assert.AreEqual("22.12 ℃", viewModel.TemperatureText);
         }
     }
 }
