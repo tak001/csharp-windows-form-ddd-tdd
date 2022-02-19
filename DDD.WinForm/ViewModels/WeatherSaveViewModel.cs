4c8bf12317c5481261c758cc5fbafe436e334475
tak001 <twitwitwi2@yahoo.co.jp>
2022-02-19 22:34:58 +0900

    feat:34-2 Save画面の実装

---
+++ DDD.WinForm/ViewModels/WeatherSaveViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DDD.WinForm.ViewModels
+{
+    public class WeatherSaveViewModel : ViewModelBase
+    {
+        public WeatherSaveViewModel()
+        {
+            DataDateValue = DateTime.Now;
+        }
+
+        public object SelectedAreaId { get; set; }
+        public DateTime DataDateValue { get; set; }
+        public object SelectedCondition { get; set; }
+        public string TemperatureText { get; set; }
+    }
+}
---
+++ DDDTest.Tests/WeatherSaveViewModelTest.cs
@@ -0,0 +1,21 @@
+﻿using DDD.WinForm.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DDDTest.Tests
+{
+    [TestClass]
+    public class WeatherSaveViewModelTest
+    {
+
+        [TestMethod]
+        public void 天気登録シナリオ()
+        {
+            var viewModel = new WeatherSaveViewModel();
+            viewModel.SelectedAreaId.IsNull();
+            viewModel.DataDateValue.Is(Convert.ToDateTime("2018/01/01 12:24:56"));
+            viewModel.SelectedCondition.Is(1);
+            viewModel.TemperatureText.Is("");
+        }
+    }
+}
